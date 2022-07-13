315a996cb4387cfb5d29fcf7592359f7d8f4768c
highlander santos <highlander@lanlink.com.br>
2022-07-12 23:44:29 -0300

    feat: add reducer ts

--- src/models/models.ts
+++ src/models/models.ts
@@ -3,3 +3,33 @@ export interface Todo {
   todo: string;
   isDone: boolean;
 }
+
+// import { useReducer } from "react";
+
+// type Actions =
+//   | { type: "add"; payload: string }
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number };
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//   switch (action.type) {
+//     case "add":
+//       return [
+//         ...state,
+//         { id: Date.now(), todo: action.payload, isDone: false },
+//       ];
+//     case "remove":
+//       return state.filter((todo) => todo.id !== action.payload);
+//     case "done":
+//       return state.map((todo) =>
+//         todo.id !== action.payload ? { ...todo, isDone: !todo.isDone } : ""
+//       );
+//     default:
+//       return state;
+//   }
+// };
+
+// export const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+//   return <div/>;
+// };
